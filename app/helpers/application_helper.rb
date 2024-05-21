6b85c0e5b7006134e52c4518920b40e5774f1932
Dioegox <degalvez@miuandes.cl>
2024-05-21 17:19:58 -0400

    pagina de inicio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def time_to_grid_position(time_str)
+    hour, minute = time_str.split(':').map(&:to_i)
+    ((hour - 7) * 2) + (minute / 30) + 2 # Adjust the row start position (7:00 should start at row 2)
+  end
+
+  def day_to_grid_column(day)
+    days = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
+    day_index = days.index(day)
+    if day_index.nil?
+      Rails.logger.warn "Invalid day value: #{day}"
+      return 1 # Default column (could be adjusted based on your requirements)
+    end
+    day_index + 2
+  end
 end
