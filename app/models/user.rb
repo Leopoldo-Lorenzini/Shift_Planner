16452c08c7a2b2583d9da98197334c88130e6b49
daniel9091 <djguzman1@miuandes.cl>
2024-06-01 11:16:44 -0400

    modificacion user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
 class User < ApplicationRecord
+    # Seguridad
     has_secure_password
+    # Relaciones
     has_many :itineraries, dependent: :destroy
     has_many :section_groups, dependent: :destroy
-
     has_many :memberships, dependent: :destroy
     has_many :section_group, through: :memberships
-
+    # Validaciones
+    validates :name, presence: true
     validates :email, presence: true, uniqueness: true
+    validates :phone, presence: true
+    validates :password_digest, presence: true
+    # Fuciones relevantes
+    def average_driver_rating
+        return 0 if n_driver_review.zero? driver_review.to_f / n_driver_review
+    end
+    
+    def average_passenger_rating
+        return 0 if n_passenger_review.zero? passenger_review.to_f / n_passenger_review
+    end
 end
-
